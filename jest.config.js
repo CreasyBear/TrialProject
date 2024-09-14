b4acef5cebcb263ef763d6d117efe8ac3e964cf6
CreasyBear <joelchan@JC-MBP.local>
2024-09-14 20:05:38 +0800

    Remove test files and configuration

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  moduleNameMapper: {
-    '^@/components/(.*)$': '<rootDir>/src/components/$1',
-    '^@/utils/(.*)$': '<rootDir>/src/utils/$1',
-  },
-}
